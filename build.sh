beb22c87dca5f63752ab39418cef8f01e46d88db
Przemysław Cuber <przemyslawcuber@gmail.com>
2025-02-04 15:43:43 +0100

    Initial commit

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+echo "Starting building and installing..."
+
+mkdir -p build
+cd build
+
+PREFIX=$HOME
+
+cmake -G Ninja \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCURL_USE_OPENSSL=ON \
+    -DCURL_ZLIB=ON \
+    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_STATIC_LIBS=OFF \
+    -DBUILD_TESTING=OFF \
+    -DENABLE_MANUAL=OFF \
+    -DENABLE_CURL_SSL=ON \
+    ..
+
+ninja install
